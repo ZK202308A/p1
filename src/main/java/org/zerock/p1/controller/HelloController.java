280aafd7fa38b36468ac85a8cbf314a47a4c3238
zk202308a@gmail.com <zk202308a@gmail.com>
2023-12-30 10:35:33 +0900

    Initial commit

---
+++ src/main/java/org/zerock/p1/controller/HelloController.java
@@ -0,0 +1,19 @@
+package org.zerock.p1.controller;
+
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Log4j2
+public class HelloController {
+
+    @GetMapping("/hello")
+    public void doA() {
+
+        log.info("hello............");
+
+
+    }
+}
